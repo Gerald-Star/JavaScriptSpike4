a901e4be283a14b4627663b4ccc9a10162d8b2c2
Gerald-Star <ukwuomagerald@gmail.com>
2022-07-27 03:15:21 +0200

    Inequality operator vs strict Inequality operator

--- main.js
+++ main.js
@@ -31,6 +31,12 @@ console.log(testRun(false));
 //compares/ converts values of the same common type
 
 //EXAMPLE 2
+/*
+1   ==  1  // true
+1   ==  2  // false
+1   == '1' // true
+"3" ==  3  // true
+*/
 
 function equalityTest(myVal) {
     if (myVal == 10) {
@@ -53,7 +59,7 @@ function equalityTest(myVal) {
      string Equal when val is equivalent to 12.
 
      */
-// Setup
+// Setup EXAMPLE 1
 function examScore(val) {
     if (val == 12) { // Change this line
       return "Next Class";
@@ -62,12 +68,53 @@ function examScore(val) {
   }
   
   console.log(examScore(10));
-  //output Passed
+  //output Repeat
+
+//EXAMPLE 2
+  function classExamScore(val) {
+    if (val == "12") { // Change this line
+      return "Promoted";
+    }
+    return "Not Promoted";
+  }
+  
+  console.log(classExamScore("12"));
+  //output Promoted
+
+
+  //EXAMPLE 3
+  function classExamScoreNo(val) {
+    if (val == "12") { // Change this line
+      return "Promoted To Next Class";
+    }
+    return "Not Promoted To Next Class";
+  }
+  
+  console.log(classExamScoreNo("14"));
+
+
+  //EXAMPLE 4 Compare to Equality Operator ==
+
+  function testStrictMan(valMan) {
+    if (valMan == "7") { // Change this line
+      return "Good Job";
+    }
+    return "Study Hard";
+  }
+  
+  console.log(testStrictMan(7));
+
 
   // STRICT OPERATOR
   /* Use the strict equality operator in the if statement 
   so the function will return the string Equal when val 
   is strictly equal to 7.*/
+
+
+  /*Examples
+
+3 ===  3  // true
+3 === '3' // false*/
 
   // EXAMPLE 1
 function testStrict(val) {
@@ -92,16 +139,7 @@ function testStrict(val) {
   console.log(testStrictCheck(7));
 
 
-  //EXAMPLE 3 Compare to Equality Operator ==
-
-  function testStrictMan(valMan) {
-    if (valMan == "7") { // Change this line
-      return "Good Job";
-    }
-    return "Study Hard";
-  }
   
-  console.log(testStrictMan(7));
 
   //example another 
   //strict equality operator that returns passed and fail. 
@@ -111,11 +149,12 @@ function testStrict(val) {
         return "Passed"; 
     } else
     if (student === "10"){
-        return "Failed"
+        return "Another Exam"
     }
+    return "No Exam"
   }
   console.log(testExamScore(20));
-  console.log(testExamScore("10")); 
+  console.log(testExamScore("7")); 
   //without the exact gives undefined
   
     
@@ -128,6 +167,7 @@ function testStrict(val) {
     return "False";
   }
   console.log(compareEquality(10, "10"));
+  // output strict Equality = false
 
   //Example 2
 
@@ -139,3 +179,114 @@ function testStrict(val) {
   }
 
   console.log(compareDataTypes (10 , "10"));
+  //output Equality = True
+
+
+  //TOPIC Inequality Operator
+
+  /*
+  inequality operator (!=) is the opposite of the 
+  equality operator. It means not equal and returns
+   false where equality would return true
+
+1 !=  2    // true <br>
+1 != "1"   // false <br>
+1 != '1'   // false <br>
+1 != true  // false <br>
+0 != false // false <br>
+  */
+//Opposite of equality operator
+
+// Setup
+
+/* Example 
+Add the inequality operator != in the if statement 
+so that the function will return the string Not Equal 
+when val is not equivalent to 99
+*/
+function testNotEqual(val) {
+    if (val != 99) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  console.log(testNotEqual(10));
+
+
+  //EXAMPLE 2
+
+  function retestEqual(val) {
+    if (val != "99") { // Change this line
+      return "Same";
+    }
+    return "Not Same";
+  }
+  
+  console.log(retestEqual(10));
+
+  
+  //EXAMPLE 3
+
+  function testEqual(val) {
+    if (val != "99") { // Change this line
+      return "Right";
+    }
+    return "Not Right";
+  }
+  
+  console.log(testEqual(99));
+
+
+//EXAMPLE 4
+
+function moreTest(val) {
+    if (val != 10) { // Change this line
+      return "Good Score";
+    }
+    return " Bad Score";
+  }
+  
+  console.log(moreTest(10));
+//output Match
+
+
+/* STRICT INEQUALITY OPERATOR
+The strict inequality operator (!==)
+ is the logical opposite of the strict equality operator
+*/
+//Examples
+/*
+3 !==  3  // false
+3 !== '3' // true
+4 !==  3  // true
+
+
+Add the strict inequality operator to the if statement
+ so the function will return the string Not Equal when 
+ val is not strictly equal to 17
+*/
+
+// Example 1
+function testStrictNotEqual(val) {
+    if (val !== 17) { // Change this line
+      return "True";
+    }
+    return "false";
+  }
+  
+   console.log(testStrictNotEqual(17));
+  //output False
+
+//Example 2
+  function testStrict(val) {
+    if (val !== "17") { // Change this line
+      return "True";
+    }
+    return "Not True";
+  }
+  
+  console.log(testStrict(17));
+  //output True
+
+
